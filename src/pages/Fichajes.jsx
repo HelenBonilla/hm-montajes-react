84d9f245f76ce7ebe9c9ee1c99a368dd9efcd09c
Helen Bonilla <helenbonillag@gmail.com>
2024-02-06 16:14:02 -0500

    Actualizacion del layout de la app

---
+++ src/pages/Fichajes.jsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+export function Fichajes() {
+  return (
+    <Container>
+      <h1>Fichajes</h1>
+    </Container>
+  );
+}
+const Container = styled.div`
+ height:100vh;`;
---
+++ src/routers/routes.jsx
@@ -0,0 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import { Home } from "../pages/Home";
+import { Trabajadores } from "../pages/Trabajadores";
+import { Fichajes } from "../pages/Fichajes";
+import { Liquidaciones } from "../pages/Liquidaciones";
+import { Nomina } from "../pages/Nomina";
+export function MyRoutes() {
+  return (
+   
+     
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Fichajes" element={<Fichajes />} />
+        <Route path="/Trabajadores" element={<Trabajadores />} />
+        <Route path="/Liquidaciones" element={<Liquidaciones />} />
+        <Route path="/Nomina" element={<Nomina />} />
+      </Routes>
+    
+  );
+}
