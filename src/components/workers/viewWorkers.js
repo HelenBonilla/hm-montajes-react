47bc43366209dbfc9a886061e6274ec87e095fcd
Helen Bonilla <helenbonillag@gmail.com>
2024-02-14 15:40:25 -0500

    Vista de trabajadores

--- src/components/workers/viewWorkers.js
+++ src/components/workers/viewWorkers.js
@@ -1,49 +1,145 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import TextField from '@mui/material/TextField';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+import { format } from 'date-fns';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { Container } from '@mui/material';
+import { Box } from '@mui/material';
+import MUIDataTable from 'mui-datatables';
+
+const getMuiTheme = () =>
+    createTheme({
+      components: {
+        MUIDataTableHeadCell: {
+          styleOverrides:{ 
+            root: {
+            backgroundColor: '#81d4fa',
+          }}
+        }
+        
+      }
+});
+
 
 export default function ViewWorkers() {
-  const [open, setOpen] = React.useState(false);
+  const { id } = useParams();
+  const [ worker, setWorker ] = useState({});
+  const endpoint = `http://127.0.0.1:8000/workers/api/v1/workers/${id}/`;
 
-  const handleClickOpen = () => {
-    setOpen(true);
-  };
 
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const getData = async () => {
+    await axios.get(endpoint).then((response) => {
+      const data = response.data
+      console.log(data)
+      setWorker(data)
+    })
+  }
+
+  useEffect(()=>{
+    getData()
+  },[]) 
+
+  const columns = [
+    {
+        name: "id",
+        label: "id"
+    },
+    {
+        name: "folder_number",
+        label: "Legajo"
+    },
+    {
+        name: "date_signed",
+        label: "Fecha Fichada",
+        options: {
+            customBodyRender: (value) => {
+              const fechaFormateada = format(new Date(value), 'yyyy-MM-dd h:mm a');
+              return (
+                <span>{fechaFormateada}</span>
+              );
+            }
+        }
+    },
+    {
+        name: "normalized_date_signed",
+        label: "Fecha Normalizada",
+        options: {
+            customBodyRender: (value) => {
+              const fechaFormateada = format(new Date(value), 'yyyy-MM-dd h:mm a');
+              return (
+                <span>{fechaFormateada}</span>
+              );
+            }
+        }
+    },
+    {
+        name: "signed_type",
+        label: "Tipo",
+        options: {
+            customBodyRender: (value) => {
+                return (
+                    <span>{value === 'E' ? 'Entrada': 'Salida'}</span>
+                );
+            }
+        }
+    },
+    {
+        name: "door",
+        label: "Puerta"
+    },
+    {
+        name: "contract_number",
+        label: "Número de contrato"
+    },
+]
+    
+const options = {
+    filterType: 'checkbox',
+    responsive: true,
+    filter: false,
+    selectableRows: false,
+    tableBodyHeight: 440,
+    elevation: 10, 
+    textLabels: {    
+        toolbar: {
+            search: "Buscar fichaje",
+            downloadCsv: "Descargar Excel",
+            print: "Imprimir lista",
+            viewColumns: "Ver Columnas",
+        },
+        pagination: {
+            next: "Siguiente Página",
+            previous: "Página anterior",
+            rowsPerPage: "Filas por páginas:",
+        },
+        body: {
+            noMatch: "Lo sentimos, no se encontraron registros coincidentes",
+            columnHeaderTooltip: column => `${column.label}`
+        },
+        selectedRows: {
+            text: "Columnas seleccionadas",
+            delete: "Eliminar fichaje",
+            deleteAria: "Delete Selected Rows",
+        },
+    
+    },
+}
+
 
   return (
-    <>
-      <Button variant="outlined" onClick={handleClickOpen}>
-        Open form dialog
-      </Button>
-      <Dialog
-        open={open}
-        onClose={handleClose}
-      >
-        <DialogTitle>Información detallada del trabajador</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            required
-            margin="dense"
-            id="name"
-            name="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose}>Cancelar</Button>
-        </DialogActions>
-      </Dialog>
-    </>
-  );
+    <ThemeProvider theme={getMuiTheme()}>
+        <Container maxWidth="xl">
+            <Box sx={{py: "1px", my: 2}}>
+              <h2>{worker.name}</h2>
+              <h3>{worker.document}</h3>
+            </Box>
+            <MUIDataTable 
+                title="Lista de fichajes"
+                data={worker.signings}
+                columns={columns}
+                options={options}
+            />
+        </Container>
+    </ThemeProvider>
+)
 }
