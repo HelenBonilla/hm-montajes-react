23aca07d676684562bb0a62d46e6435a9022cbd7
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-02-22 15:53:54 -0500

    arreglada la localizacion de los datepicker

--- src/components/common/dateRange.jsx
+++ src/components/common/dateRange.jsx
@@ -4,12 +4,13 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import MinimizeIcon from '@mui/icons-material/Minimize';
-import { TextField } from '@mui/material';
+import 'dayjs/locale/es';
 
 export default function DateRangePicker() {
+    const [locale, setLocale] = React.useState('es');
 
     return (
-        <LocalizationProvider dateAdapter={AdapterDayjs} >
+        <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale={locale}>
             <DemoContainer components={['DatePicker']} >
                 <DatePicker label="Fecha de inicio"  />
                     <MinimizeIcon color="primary"/>
