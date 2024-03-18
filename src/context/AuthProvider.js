55425c074508e6e0db00d15e7833b8fdeba4d24a
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-03-18 14:27:25 -0500

    fixed AuthProvider to verifyToken on loading and use correctly the localStorage

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,28 +1,40 @@
-import {
-    createContext,
-    useMemo,
-    useEffect,
-    useState,
-} from 'react';
+import { createContext, useEffect, useMemo } from 'react';
+import { useNavigate } from "react-router-dom";
+import axios from '../api/axios';
+import { useLocalStorage } from '../hooks/useLocalStorage';
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState(JSON.parse(localStorage.getItem('auth')) ?? {})
+    const [auth, setAuth] = useLocalStorage('auth', {})
+    const navigate = useNavigate();
+
+    const logout = () => {
+        setAuth({});
+        navigate("/auth/login", { replace: true });
+    };
 
     useEffect(() => {
-        if (Object.keys(auth).length) {
-            localStorage.setItem('auth', JSON.stringify(auth));
-        } else {
-            localStorage.removeItem('auth');
+        const verifyToken = async (token) => {
+            try {
+                const response = await axios.post('/api/token/verify/', {
+                    token: auth?.refreshToken
+                })
+            } catch (error) {
+                // BAD REQUEST: either the token was empty or it was invalid
+                logout()
+            }
         }
-    }, [auth])
+
+        verifyToken()
+    }, [])
 
     const contextValue = useMemo(
         () => {
             return {
                 auth,
                 setAuth,
+                logout,
             }
         },
         [auth]
